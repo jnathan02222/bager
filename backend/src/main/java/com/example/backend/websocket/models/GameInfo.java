5877b203d4d6f9ef0a6acae98d4e90583373da4a
Nathan <test@gmail.com>
2024-09-08 17:54:05 -0400

    Undo fix

--- backend/src/main/java/com/example/backend/websocket/models/GameInfo.java
+++ backend/src/main/java/com/example/backend/websocket/models/GameInfo.java
@@ -228,6 +228,8 @@ public class GameInfo{
 
        
         if(playerQueue.indexOf(p) == selectedPlayer - 1 && gameStarted){
+            
+            selectedPlayer -= 1;
             updateGameState(new GameState(GameState.TALLY_SCORE), roomId, multiplayerService, messagingTemplate);
         }
 
