72367e9bb5657c8aa0e3109d76b8493a4011c59b
Nathan <test@gmail.com>
2024-09-08 16:50:17 -0400

    Shell script change

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ sudo ./gradlew bootRun &
 cd /home/n25jiang/bager/website
 npm start &
 cd /home/n25jiang/bager/httpProxy
-node index.js &
+sudo /home/n25jiang/.nvm/versions/node/v20.17.0/bin/node index.js &
+
+
