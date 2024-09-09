3d7adab9613218c2aa03683f2e316095d19efa78
Nathan <test@gmail.com>
2024-09-09 09:40:42 -0400

    nginx changes

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd /home/n25jiang/bager/backend
-sudo ./gradlew bootRun &
-cd /home/n25jiang/bager/website
-npm start &
-cd /home/n25jiang/bager/httpProxy
-sudo /home/n25jiang/.nvm/versions/node/v20.17.0/bin/node index.js &
+sudo ./gradlew bootRun disown &
 
 
