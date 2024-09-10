c9ebf4a226f6a38bafea07858139db936d81031b
jnathan02222 <153697998+jnathan02222@users.noreply.github.com>
2024-09-10 12:04:31 -0400

    Update WebSocketConfig.java

--- backend/src/main/java/com/example/backend/websocket/WebSocketConfig.java
+++ backend/src/main/java/com/example/backend/websocket/WebSocketConfig.java
@@ -25,6 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     //This is how a STOMP connection will be established
     registry.addEndpoint("/ws/connect");
+    registry.setAllowedOrigins("https://floppyfingers.online/")
   }
 
 }
